03744068a6cd21db1bc0deeaa49f33227bce0e7d
antonkrupin <krupin_anton@mail.ru>
2021-08-11 13:56:43 +0300

    Add files via upload

---
+++ myFilter.js
@@ -0,0 +1,16 @@
+const myFilter = (arr, template) => {
+    let templateKeys = Object.keys(template);
+    let templateMatches = arr.filter(function (e) {
+        let counter = 0;
+        for (key of templateKeys) {
+            if (e[key] === template[key]) {
+                counter += 1;
+            }
+        }
+        if (counter === templateKeys.length) {
+            return e
+        }
+    })
+
+    return templateMatches;
+}
